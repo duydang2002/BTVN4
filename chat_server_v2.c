7c29fbb7a595c59c5c3084500f985ffb6a258b00
duydang2002 <76654292+duydang2002@users.noreply.github.com>
2023-05-13 17:55:30 +0700

    Add files via upload

---
+++ chat_server_v2.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <poll.h>
+
+#define MAX_CLIENTS 64
+
+int main() 
+{
+    int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listener == -1)
+    {
+        perror("socket() failed");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(9000);
+
+    if (bind(listener, (struct sockaddr *)&addr, sizeof(addr))) 
+    {
+        perror("bind() failed");
+        return 1;
+    }
+
+    if (listen(listener, 5)) 
+    {
+        perror("listen() failed");
+        return 1;
+    }
+
+    struct pollfd fds[MAX_CLIENTS];
+    int nfds = 1;
+
+    fds[0].fd = listener;
+    fds[0].events = POLLIN;
+
+    char buf[256];
+
+    int users[64];      // Mang socket client da dang nhap
+    char *user_ids[64]; // Mang luu tru id cua client da dang nhap
+    int num_users = 0;  // So client da dang nhap
+
+    while (1)
+    {
+        int ret = poll(fds, nfds, -1);
+        if (ret < 0)
+        {
+            perror("poll() failed");
+            break;
+        }
+
+        printf("ret = %d\n", ret);
+
+        if (fds[0].revents & POLLIN)
+        {
+            int client = accept(listener, NULL, NULL);
+            if (nfds < MAX_CLIENTS)
+            {
+                printf("New client connected: %d\n", client);
+                 char *msg = "Enter the format 'client_id: client_name' to chat ";
+                fds[nfds].fd = client;
+                fds[nfds].events = POLLIN;
+                send(fds[nfds].fd,msg,strlen(msg),0);
+                nfds++;
+               
+                
+            }
+            else
+            {
+                printf("Too many connections\n");
+                close(client);
+            }
+        }
+
+        for (int i = 1; i < nfds; i++)
+            if (fds[i].revents & POLLIN)
+            {
+                ret = recv(fds[i].fd, buf, sizeof(buf), 0);
+                if (ret <= 0)
+                {
+                    printf("Client %d disconnected.\n", fds[i].fd);
+                    close(fds[i].fd);
+                    
+                    // Xoa phan tu i khoi mang
+                    if (i < nfds - 1){
+                        fds[i] = fds[nfds - 1];
+                    }
+                        nfds--;
+                        i--;
+                    users[num_users]=0;
+                    num_users--;
+                }
+                else
+                {
+                    buf[ret] = 0;
+                    printf("Received from %d: %s\n", fds[i].fd, buf);
+                    int client = fds[i].fd;
+
+                        int j = 0;
+                        for (; j < num_users; j++)
+                            if (users[j] == client) break;
+
+                        if (j == num_users)
+                        {
+                            // Chua dang nhap
+                            // Xu ly cu phap yeu cau dang nhap
+                            
+                            char cmd[32], id[32], tmp[32];
+                            ret = sscanf(buf, "%s%s%s", cmd, id, tmp);
+                            if (ret == 2)
+                            {
+                                if (strcmp(cmd, "client_id:") == 0)
+                                {
+                                    char *msg = "Dung cu phap. Gui tin nhan.(neu muon gui tin nhan rieng thi dung cu pha [name] msg)\n";
+                                    send(client, msg, strlen(msg), 0);
+
+                                    int k = 0;
+                                    for (; k < num_users; k++)
+                                        if (strcmp(user_ids[k], id) == 0) break;
+                                    
+                                    if (k < num_users)
+                                    {
+                                        char *msg = "ID da ton tai. Yeu cau nhap lai.\n";
+                                        send(client, msg, strlen(msg), 0);
+                                    }
+                                    else
+                                    {
+                                        users[num_users] = client;
+                                        user_ids[num_users] = malloc(strlen(id) + 1);
+                                        strcpy(user_ids[num_users], id);
+                                        num_users++;
+                                    }                                    
+                                }
+                                else
+                                {
+                                    char *msg = "Nhap sai. Yeu cau nhap lai.\n";
+                                    send(client, msg, strlen(msg), 0);
+                                }
+                            }
+                            else
+                            {
+                                char *msg = "Nhap sai. Yeu cau nhap lai.\n";
+                                send(client, msg, strlen(msg), 0);
+                            }
+                        }
+                        else
+                        {
+                            // Da dang nhap
+                            char sendbuf[512];
+                            char cmd[32],temp[32],name[32],msg[256];
+                            
+                            // Kiem tra format [ten] msg
+                            if (strchr(buf,'[')==buf && strchr(buf,']')>buf+1){
+                            int ret1 = sscanf(buf,"%s%s",cmd,temp);
+                            printf("%s %s\n",cmd,temp);
+                            if (ret1==2){
+                                
+                                // lay ten tu [ten] roi kiem tra
+                                sscanf(cmd,"[%[^]]]",name);
+                                strcpy(msg,strchr(buf,']')+1);
+                                printf("%s",msg);
+                                for (int k=0;k<num_users;k++){
+                                    if (strcmp(user_ids[k],name)==0){
+
+                                        sprintf(sendbuf, "[%s]: %s", user_ids[j], msg);
+                                        send(users[k], sendbuf, strlen(sendbuf), 0);
+                                    }
+                                }
+                                // printf("%s %s",cmd,msg);
+                                continue;
+                            }
+                            }
+                            // if (strcmp(buf,"/name\n")==0){
+                            //   for (int k=0;k<num_users;k++){
+                            //     send(users[k], user_ids[k], strlen(user_ids[k]), 0);
+                            //   }
+                            //   continue;  
+                            // }
+                            sprintf(sendbuf, "%s: %s", user_ids[j], buf);
+                            for (int k = 0; k < num_users; k++)
+                                if (users[k] != client)
+                                    send(users[k], sendbuf, strlen(sendbuf), 0);
+                        }
+                }
+            }
+    }
+
+    close(listener);    
+
+    return 0;
+}
---
+++ telnet_server.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <poll.h>
+
+#define MAX_CLIENTS 64
+#define MAX_LEN 128
+#define MAX_LINE 128
+
+int main() 
+{
+    int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listener == -1)
+    {
+        perror("socket() failed");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(9000);
+
+    if (bind(listener, (struct sockaddr *)&addr, sizeof(addr))) 
+    {
+        perror("bind() failed");
+        return 1;
+    }
+
+    if (listen(listener, 5)) 
+    {
+        perror("listen() failed");
+        return 1;
+    }
+   FILE *f = fopen("database.txt","r");
+    if (f==NULL){
+        printf("Error opening file\n");
+        return 0;
+        }
+    char data[MAX_LINE][MAX_LEN];
+    int line =0;
+    while (!feof(f) && !ferror(f))
+    {
+       if (fgets(data[line],MAX_LEN,f)!=NULL)
+        line++;
+    }
+   for (int i = 0; i < line; i++) {
+    data[i][strcspn(data[i], "\n")] = '\0'; // Loại bỏ ký tự '\n'
+}
+    fclose(f);
+
+                                
+
+    struct pollfd fds[MAX_CLIENTS];
+    int nfds = 1;
+
+    fds[0].fd = listener;
+    fds[0].events = POLLIN;
+
+    char buf[256];
+
+    int users[64];      // Mang socket client da dang nhap
+    char *user_ids[64]; // Mang luu tru id cua client da dang nhap
+    int num_users = 0;  // So client da dang nhap
+
+    while (1)
+    {
+        int ret = poll(fds, nfds, -1);
+        if (ret < 0)
+        {
+            perror("poll() failed");
+            break;
+        }
+
+        printf("ret = %d\n", ret);
+
+        if (fds[0].revents & POLLIN)
+        {
+            int client = accept(listener, NULL, NULL);
+            if (nfds < MAX_CLIENTS)
+            {
+                printf("New client connected: %d\n", client);
+                 char *msg = "Enter the format 'UserName Password' to login ";
+                fds[nfds].fd = client;
+                fds[nfds].events = POLLIN;
+                send(fds[nfds].fd,msg,strlen(msg),0);
+                nfds++;
+               
+                
+            }
+            else
+            {
+                printf("Too many connections\n");
+                close(client);
+            }
+        }
+
+        for (int i = 1; i < nfds; i++)
+            if (fds[i].revents & POLLIN)
+            {
+                ret = recv(fds[i].fd, buf, sizeof(buf), 0);
+                if (ret <= 0)
+                {
+                    printf("Client %d disconnected.\n", fds[i].fd);
+                    close(fds[i].fd);
+                    
+                    // Xoa phan tu i khoi mang
+                    if (i < nfds - 1){
+                        fds[i] = fds[nfds - 1];
+                    }
+                        nfds--;
+                        i--;
+                        users[num_users]=0;
+                        num_users--;
+                }
+                else
+                {
+                    // buf[ret] = 0;
+                    buf[strcspn(buf,"\n")]= '\0';
+                    // printf ("%ld",strcspn(buf,'\n'));
+                    printf("Received from %d: %s\n", fds[i].fd, buf);
+                    int client = fds[i].fd;
+
+                        int j = 0;
+                        for (; j < num_users; j++)
+                            if (users[j] == client) break;
+
+                        if (j == num_users)
+                        {
+                            // Chua dang nhap
+                            // Xu ly cu phap yeu cau dang nhap
+                            
+                            char account[32], passW[32], tmp[32];
+                            ret = sscanf(buf, "%s%s%s", account, passW, tmp);
+                            if (ret == 2)
+                            {
+                                int found =0;
+                                for (int i=0;i<line;i++){
+                                char acc[64], pass[64];;
+                                sscanf(data[i],"%s%s",acc,pass);
+                                if (strcmp(account,acc)==0 && strcmp(passW,pass)==0){
+                                    found =1;
+                                    break;
+                                }
+}
+                                // Dang nhap thanh cong
+                                if (found){
+                                    users[num_users]=client;
+                                    user_ids[num_users]= malloc(strlen(account)+1);
+                                    strcpy(user_ids[num_users],account);
+                                    num_users++;
+                                    char *msg = "Dang Nhap Thanh Cong\n";
+                                    send(client, msg, strlen(msg), 0);
+                                }
+                                else{
+                                    char *msg = "Nhap sai. Yeu cau nhap lai.\n";
+                                    send(client, msg, strlen(msg), 0);
+                                }
+    
+                            }
+                            else
+                            {
+                                char *msg = "Nhap sai. Yeu cau nhap lai.\n";
+                                send(client, msg, strlen(msg), 0);
+                            }
+                        }
+                        else
+                        {
+                            // Da dang nhap
+                            char *command = malloc(strlen(buf)+1);
+                            sprintf(command,"%s > out.txt",buf);
+                           
+                            int result = system(command);
+                            
+                            if (result == -1) {
+                                char *msg = "Failed to execute command\n";
+                                send(client, msg, strlen(msg), 0);
+                            }
+                            
+                            // Kiem tra ket qua thuc thi lenh
+                            if (result == 0) {
+                                char *msg = "Command executed successfully.\n";
+                                send(client, msg, strlen(msg), 0);
+                                char result[256];
+                                FILE *file = fopen("out.txt","r");
+                                if (file == NULL) {
+                                    perror("Cannot Opening File");
+                                    return 0;
+                                }
+                                
+                                while (!feof(f) && !ferror(f))
+                                {
+                                    if  (fgets(result,sizeof(result),file)!= NULL){
+                                    send(client,result,strlen(result),0);
+                                    }
+                                }
+                                fclose(file);
+                              
+                            } else {
+                                char *msg = "Command execution failed.\n";
+                                send(client, msg, strlen(msg), 0);
+                                
+                            }
+    
+                        }
+                }
+            }
+    }
+
+    close(listener);    
+
+    return 0;
+}
